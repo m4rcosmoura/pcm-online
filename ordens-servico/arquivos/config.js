e8f6b40e21640cb3d2c354c7fa2a43c235be05f8
Marcos Moura <85023343+m4rcosmoura@users.noreply.github.com>
2026-03-25 17:09:20 -0300

    Delete ordens-servico/arquivos directory

--- ordens-servico/arquivos/config.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = {
-  SUPABASE_URL: "https://brnxwzhhlndiwnhfvrrm.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJybnh3emhobG5kaXduaGZ2cnJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5NDg0NDYsImV4cCI6MjA4OTUyNDQ0Nn0.jxEdljr-izdEEoDSq--lVu5Bzl-re9-aQLE07l3lHBA"
-};
